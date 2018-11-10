b577a8e386455b4f9a0ef2c57bc68bd802127a05
Hoang Mai <hoang.mai@smartt.com>
2018-11-10 15:56:53 -0800

    modify main

--- app/src/main/java/com/example/amynguyen/foodlover/MainActivity.java
+++ app/src/main/java/com/example/amynguyen/foodlover/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.amynguyen.foodlover;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.amynguyen.foodlover.yelpAPI.YelpHelper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity  implements LocationListener {
 
     private TextView mTextMessage;
+    private LocationManager mLocationManager;
+    private static final String[] LOCATION_PERMS={
+            ACCESS_FINE_LOCATION,
+            ACCESS_COARSE_LOCATION
+    };
+    private static final int LOCATION_REQUEST=1340;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -30,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +61,79 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!canAccessLocation()) {
+            ActivityCompat.requestPermissions(this, LOCATION_PERMS, LOCATION_REQUEST);
+        }
+        this.getBusinessList();
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        // this.getBusinessList();
+        if (location != null) {
+            Log.v("Location Changed", location.getLatitude() + " and " + location.getLongitude());
+            mLocationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        int index = 0;
+        Map<String, Integer> PermissionsMap = new HashMap<String, Integer>();
+        for (String permission : permissions){
+            PermissionsMap.put(permission, grantResults[index]);
+            index++;
+        }
+        if((PermissionsMap.get(ACCESS_FINE_LOCATION) != 0)
+                || PermissionsMap.get(ACCESS_COARSE_LOCATION) != 0){
+            Toast.makeText(this, "Location permission is a must", Toast.LENGTH_SHORT).show();
+            finish();
+        }else {
+            this.getBusinessList();
+        }
+    }
+    public void getBusinessList() {
+        try {
+            // Get last known location
+            Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            // Execute if its the first location
+                // mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 10, this);
+                String coordinate = location.getLatitude() + "," + location.getLongitude();
+                System.out.println(coordinate);
+                // Print// create Yelp Helper instance
+                final YelpHelper yelpHelper = new YelpHelper();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        // execute command
+                        JsonObject result = yelpHelper.getBusinessQuery();
+                    }
+                };
+                new Thread(runnable).start();
+        } catch (SecurityException ex) {
+            System.out.println(ex);
+        }
+    }
+    private boolean canAccessLocation() {
+        return(hasPermission(ACCESS_FINE_LOCATION));
+    }
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
+    }
 }
---
+++ app/src/main/java/com/example/amynguyen/foodlover/yelpAPI/YelpHelper.java
@@ -0,0 +1,70 @@
+package com.example.amynguyen.foodlover.yelpAPI;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+import com.rapidapi.rapidconnect.Argument;
+import com.rapidapi.rapidconnect.RapidApiConnect;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+
+public class YelpHelper {
+    public final static String APP_NAME = "default-application_5be34bdde4b08725af2ad174";
+    public final static String APP_ID = "9ba414c5-557c-45e5-a1b6-0218c9aa2edf";
+    public final static String YIELP_API_KEY = "MsBMFjvBxbbjORnbOCk0hBaFONlDNDt0C1uUGik3WC3N_r0IfLGoDqhdmNrUSUgrfYXVd-nNTodyE4E3rTdOgt5G58rFlCYEMpMPGyHT7pvCA2-nW_UkzazS_8bjW3Yx";
+
+    RapidApiConnect connect;
+    Map<String, Argument> body;
+    Gson gson;
+
+    String term = "";
+    Boolean openNow = true;
+    Double radius = 0.00;
+    String location = null;
+    String coordinate = null;
+
+    public YelpHelper() {
+        this.connect = new RapidApiConnect(APP_NAME, APP_ID);
+        this.body = new HashMap<String, Argument>();
+        body.put("accessToken", new Argument("data", YIELP_API_KEY));
+        gson = new Gson();
+    }
+    public JsonObject getBusinessQuery() {
+        if(location == null && coordinate == null) return null;
+        if(location != null) body.put("location", new Argument("data", location));
+        else body.put("coordinate", new Argument("data", coordinate));
+        body.put("term", new Argument("data", term));
+        body.put("openNow", new Argument("data", String.valueOf(openNow)));
+        if(radius > 0 ) body.put("radius", new Argument("data", String.valueOf(radius)));
+        try {
+            Map<String, Object> response = connect.call("YelpAPI", "getBusinesses", body);
+            if(response.get("success") != null) {
+                LinkedTreeMap<?,?> result = (LinkedTreeMap) response.get("success");
+                // JsonObject jsonObject = gson.toJsonTree(yourMap).getAsJsonObject();
+                JsonObject data = gson.toJsonTree(result).getAsJsonObject();
+                // JsonArray array = data.getAsJsonObject().getAsJsonArray("businesses");
+                System.out.println("success: " + data.getAsJsonObject().get("total"));
+                return data;
+                // return null;
+            } else{
+                System.out.println("error: " + response);
+                return null;
+            }
+        } catch(Exception e){
+            System.out.println("Error: " + e);
+            return null;
+        }
+    }
+
+
+}
